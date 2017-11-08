3d318bbadde777a88501603c28c09acd56d4409c
Sam Stokes <me@samstokes.co.uk>
2017-11-08 17:32:06 -0600

    Add tests for the testing support (whoa, meta)

---
+++ test/test_test.rb
@@ -0,0 +1,15 @@
+require 'libhoney'
+
+# Tests for TestClient, which provides support for testing instrumented code.
+class LibhoneyTestTest < Minitest::Test
+  def test_test_client
+    fakehoney = Libhoney::TestClient.new
+
+    fakehoney.send_now('argle' => 'bargle')
+
+    assert_equal 1, fakehoney.events.size
+
+    event = fakehoney.events[0]
+    assert_equal 'bargle', event.data['argle']
+  end
+end
