4147cc3caa5e6d1fcb81e76282cbb83990275ab1
Shane Becker <veganstraightedge@gmail.com>
2018-09-21 00:08:03 -0700

    Expand abbrevs for disambiguation

--- example/fact.rb
+++ example/fact.rb
@@ -16,27 +16,28 @@ end
 
 # run factorial. libh_builder comes with some fields already populated
 # (namely, "version", "num_threads", and "range")
-def run_fact(low, high, libh_builder)
+def factorial(low, high, libh_builder)
   (low..high).each do |i|
-    ev = libh_builder.event
-    ev.metadata = { fn: 'run_fact',
-                    i: i }
-    ev.with_timer('fact') do
-      res = factorial(10 + i)
-      ev.add_field('retval', res)
+    event = libh_builder.event
+    event.metadata = { fn: 'run_factorial', i: i }
+
+    event.with_timer('factorial') do
+      result = factorial(10 + i)
+      event.add_field('retval', result)
     end
-    ev.send
+
+    event.send
   end
 end
 
-def read_responses(resp_queue)
+def read_responses(response_queue)
   loop do
-    resp = resp_queue.pop
-    puts resp_queue.inspect
-    break if resp.nil?
+    response = response_queue.pop
+    puts response_queue.inspect
+    break if response.nil?
 
-    puts "Sent: Event with metadata #{resp.metadata} in #{resp.duration * 1000}ms."
-    puts "Got:  Response code #{resp.status_code}"
+    puts "Sent: Event with metadata #{response.metadata} in #{response.duration * 1000}ms."
+    puts "Got:  Response code #{response.status_code}"
     puts
   end
 end
@@ -45,7 +46,8 @@ libhoney = Libhoney::Client.new(writekey: writekey,
                                 dataset:  dataset,
                                 max_concurrent_batches: 1)
 
-resps = libhoney.responses
+responses = libhoney.responses
+
 Thread.new do
   begin
     # attach fields to top-level instance
@@ -56,8 +58,8 @@ Thread.new do
 
     # sends an event with "version", "num_threads", and "status" fields
     libhoney.send_now(status: 'starting run')
-    run_fact(1, 20, libhoney.builder(range: 'low'))
-    run_fact(31, 40, libhoney.builder(range: 'high'))
+    run_factorial(1, 20, libhoney.builder(range: 'low'))
+    run_factorial(31, 40, libhoney.builder(range: 'high'))
 
     # sends an event with "version", "num_threads", and "status" fields
     libhoney.send_now(status: 'ending run')
@@ -67,4 +69,4 @@ Thread.new do
   end
 end
 
-read_responses(resps)
+read_responses(responses)
