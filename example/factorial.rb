c59263d31658ac32325d8d5a6bfc54c6fd81d28b
Sam Stokes <me@samstokes.co.uk>
2018-11-01 21:54:48 -0500

    Make example code more representative (#33)
    * Run actual program in main thread and consume responses in background
    
    * Remove user-irrelevant and unimplemented option from example code
    
    * Catch exceptions in background thread
    
    * Show how to print response errors too

--- example/factorial.rb
+++ example/factorial.rb
@@ -30,49 +30,43 @@ def run_factorial(low, high, libh_builder)
   end
 end
 
-def read_responses(response_queue)
-  loop do
-    response = response_queue.pop
-    break if response.nil?
-
-    puts "Sent: Event with metadata #{response.metadata} in #{response.duration * 1000}ms."
-    puts "Got:  Response code #{response.status_code}"
-    puts
-  end
-end
-
 libhoney = Libhoney::Client.new(writekey: writekey,
-                                dataset:  dataset,
-                                max_concurrent_batches: 1)
-
-responses = libhoney.responses
+                                dataset:  dataset)
 
 Thread.new do
   begin
-    # attach fields to top-level instance
-    libhoney.add_field('version', '3.4.5')
+    loop do
+      response = libhoney.responses.pop
+      break if response.nil?
 
-    a_proc = proc { Thread.list.select { |thread| thread.status == 'run' }.count }
-    libhoney.add_dynamic_field('num_threads', a_proc)
-
-    event = libhoney.event
-    event.metadata = { fn: 'work_thread' }
-    event.add_field('start_time', Time.now.iso8601(3))
-    event.with_timer 'run_fact_low_dur_ms' do
-      run_factorial(1, 20, libhoney.builder(range: 'low'))
+      puts "Sent: Event with metadata #{response.metadata} in #{response.duration * 1000}ms."
+      puts "Got:  Response code #{response.status_code}"
+      puts "      #{response.error.class}: #{response.error}" if response.error
+      puts
     end
-    event.with_timer 'run_fact_high_dur_ms' do
-      run_factorial(31, 40, libhoney.builder(range: 'high'))
-    end
-    event.add_field('end_time', Time.now.iso8601(3))
-    # sends an event with "version", "num_threads", "start_time", "end_time",
-    # "run_fact_low_dur_ms", "run_fact_high_dur_ms"
-    event.send
-
-    libhoney.close
   rescue StandardError => e
-    puts e
+    puts "#{e.class} in response reader thread: #{e}"
   end
 end
 
-read_responses(responses)
+# attach fields to top-level instance
+libhoney.add_field('version', '3.4.5')
+
+a_proc = proc { Thread.list.select { |thread| thread.status == 'run' }.count }
+libhoney.add_dynamic_field('num_threads', a_proc)
+
+event = libhoney.event
+event.metadata = { fn: 'work_thread' }
+event.add_field('start_time', Time.now.iso8601(3))
+event.with_timer 'run_fact_low_dur_ms' do
+  run_factorial(1, 20, libhoney.builder(range: 'low'))
+end
+event.with_timer 'run_fact_high_dur_ms' do
+  run_factorial(31, 40, libhoney.builder(range: 'high'))
+end
+event.add_field('end_time', Time.now.iso8601(3))
+# sends an event with "version", "num_threads", "start_time", "end_time",
+# "run_fact_low_dur_ms", "run_fact_high_dur_ms"
+event.send
+
+libhoney.close
