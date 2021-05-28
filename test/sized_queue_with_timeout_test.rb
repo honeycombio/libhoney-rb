89fba390a68ea9dfc342b63d5841dc508c5637d4
Robb Kidd <robbkidd@honeycomb.io>
2021-05-28 09:11:38 -0400

    add more tests for the new queue
    Very much based on Avdi Grimm's Tapas::Queue lockstep testing.[1]
    
    [1] https://www.rubytapas.com/2013/12/05/episode-157-testing-threads-2/

--- test/sized_queue_with_timeout_test.rb
+++ test/sized_queue_with_timeout_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 require 'lockstep' # supplier of SyncThread
 require 'libhoney/sized_queue_with_timeout'
 
-class SizedQueueWithTimeoutTest < Minitest::Test
-  def test_waiting_for_an_item
+class PopFromSizedQueueWithTimeoutTest < Minitest::Test
+  def test_wait_for_available_item
     q = Libhoney::SizedQueueWithTimeout.new
     consumer = Thread.new do
       q.pop
@@ -13,15 +13,90 @@ class SizedQueueWithTimeoutTest < Minitest::Test
     assert_equal('hello', consumer.value)
   end
 
-  def test_trying_to_push_on_a_full_queue
-    q = Libhoney::SizedQueueWithTimeout.new(3)
-    _consumer = Thread.new do
-      q.pop
-      sleep 1
+  def test_timeout_waiting_for_item
+    q = Libhoney::SizedQueueWithTimeout.new
+    assert_raises Libhoney::SizedQueueWithTimeout::PopTimedOut do
+      q.pop(0.01)
     end
-    q.push(1)
-    q.push(2)
-    q.push(3)
-    q.push(4)
+  end
+end
+
+class PushToSizedQueueWithTimeoutTest < Minitest::Test
+  def test_timeout_waiting_for_space
+    size_limit = 5
+    q = Libhoney::SizedQueueWithTimeout.new(size_limit)
+    size_limit.times do |n|
+      q.push(n)
+    end
+    assert q.send(:full?)
+    assert_raises Libhoney::SizedQueueWithTimeout::PushTimedOut do
+      q.push(:nope, 0.01)
+    end
+  end
+
+  def test_wait_for_available_space
+    size_limit = 3
+
+    q = Libhoney::SizedQueueWithTimeout.new(
+      size_limit,
+      lock: FakeLock.new,
+      space_available_condition: space_available = FakeCondition.new,
+      item_available_condition: FakeCondition.new
+    )
+
+    # some pretend threads to control timing
+    producer = SyncThread.new
+    consumer = SyncThread.new
+
+    # have the item producer fill the queue to its limit
+    producer.run(ignore: [:signal]) do
+      size_limit.times do |n|
+        q.push "item #{n + 1}"
+      end
+    end
+    # the "thread" was able to go all of its work without waiting
+    assert producer.finished?
+
+    # have the item producer try to add something to the full queue
+    producer.run(ignore: [:signal]) do
+      q.push "item #{size_limit + 1}"
+    end
+    # the producer is told to wait for space available
+    assert producer.interrupted_by?(space_available, :wait)
+
+    # take one thing out of the queue
+    consumer.run do
+      q.pop
+    end
+    # confirm the consumer "sends" a signal that space is available
+    assert consumer.interrupted_by?(space_available, :signal)
+    consumer.finish
+
+    # confirm that the producer as able to finish its work after resuming
+    # from its wait
+    assert producer.resume(ignore: [:signal]).finished?
+
+    # eat the rest of the items on the queue
+    consumer.run(ignore: [:signal]) do
+      size_limit.times.map { q.pop }
+    end
+    # confirm we get the rest of the items
+    assert_equal(['item 2', 'item 3', 'item 4'], consumer.last_return_value)
+  end
+end
+
+class FakeCondition
+  def wait(timeout)
+    SyncThread.interrupt(self, :wait, timeout)
+  end
+
+  def signal
+    SyncThread.interrupt(self, :signal)
+  end
+end
+
+class FakeLock
+  def synchronize
+    yield
   end
 end
