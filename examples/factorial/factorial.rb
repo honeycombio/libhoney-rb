e55950eb471eb4d31aa334ac2d047b36a6d8d1c8
Vera Reynolds <verareynolds@honeycomb.io>
2022-11-06 12:58:39 -0500

    fix factorial example

--- examples/factorial/factorial.rb
+++ examples/factorial/factorial.rb
@@ -1,6 +1,3 @@
-lib = File.expand_path('../../lib', __dir__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
 require 'libhoney'
 
 # replace this with yours from https://ui.honeycomb.com/account
