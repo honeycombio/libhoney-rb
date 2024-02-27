f7d8301cf08d63a35f31105695d17abeda97e466
Robb Kidd <robbkidd@honeycomb.io>
2024-02-27 10:10:00 -0500

    give the regexes names and make them constants

--- lib/libhoney.rb
+++ lib/libhoney.rb
@@ -8,13 +8,15 @@ require 'libhoney/response'
 require 'libhoney/transmission'
 
 module Libhoney
+  CLASSIC_ORIGINAL_FLAVOR = Regexp.new(/\A[[:alnum:]]{32}\z/)
+  CLASSIC_V3_INGEST = Regexp.new(/\Ahc[a-z]ic_[[:alnum:]]{58}\z/)
   # Determines if the given string is a Honeycomb API key for Classic environments.
   #
   # @param api_key [String] the string to check
   # @return [Boolean] true if the string is nil or a classic API key, false otherwise
   def self.classic_api_key?(api_key)
-    api_key.nil? ||
-      api_key.match(/\A[[:alnum:]]{32}\z/) ||
-      api_key.match(/\Ahc[a-z]ic_[[:alnum:]]{58}\z/)
+    api_key.nil? || # default to classic behavior if no API key is provided
+      CLASSIC_ORIGINAL_FLAVOR.match?(api_key) ||
+      CLASSIC_V3_INGEST.match?(api_key)
   end
 end
